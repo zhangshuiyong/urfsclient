9987bc6487ac97d25abe965b8712def1592d47ca
zhangshuiyong <632386504@qq.com>
2023-07-08 16:48:26 +0800

    change ui framework to antd-vue 4

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
+<script setup lang="ts">
+
+import Upload from "./components/Upload.vue";
+
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+
+const locale = zhCN;
+
+</script>
+
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <a-config-provider :locale="locale">
+    <div class="container">
+        <h1>Welcome to Tauri!</h1>
+
+        <Upload />
+    </div>
+  </a-config-provider>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
-</script>
-<style>
-    body{
-        margin: 0;
-       min-width: 900px;
-    }
-</style>
+<style scoped>
 
+.container {
+  text-align: center;
+}
+
+</style>
--- src/components/upload.vue
+++ src/components/upload.vue
@@ -1,127 +1,55 @@
-<template>
-  <div class="uploadWrapper">
-    <el-upload class="upload-demo" drag :on-change="showText" action="#" :limit="1" :auto-upload="false">
-      <el-icon class="el-icon--upload"><upload-filled /></el-icon>
-      <div class="el-upload__text">请将文件拖拽到此处</div>
-      <template #tip>
-        <div class="el-upload__tip"></div>
-      </template>
-    </el-upload>
-    <el-row class="row-bg" justify="center">   
-      <el-col :span="4"><el-button type="primary" @click="star_upload()">点击上传</el-button></el-col>
-      
-    </el-row>
-
-    <el-row class="row-bg" justify="center">   
-      <el-col :span="4"><el-button type="primary" @click="stop_upload()">暂停上传</el-button></el-col>
-    </el-row>
-
-    <el-row class="row-bg" justify="center">   
-      <el-col :span="4"><el-button type="primary" @click="terminate_upload()">终止上传</el-button></el-col>
-    </el-row>
-
-    <el-row class="row-bg" justify="center">   
-      <el-col :span="4"><el-button type="primary" @click="get_history()">上传历史</el-button></el-col>
-    </el-row>
-    
-  </div>
-</template>
-<script lang="ts" setup>
-window.addEventListener("drop", (e) => e.preventDefault(), false);
-window.addEventListener("dragover", (e) => e.preventDefault(), false);
+<script setup lang="ts">
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
 import { info } from "tauri-plugin-log-api";
-import { ElMessage } from 'element-plus'
-let name = ref('');
-const showText=(file: { name: string; })=>{
-  name.value=file.name
-}
-async function star_upload() {
-  if(name.value==''){
-    ElMessage({
-    message: '文件不能为空',
-    type: 'warning',
-  })
-  }else{
-    ElMessage({
-    message: '开始上传',
-    type: 'success',
-  })
+import { message } from 'ant-design-vue';
 
- try{
-    await invoke("start_upload", { req :JSON.stringify({dataset_id: 'xx',server_endpoint: 'xxx' })})
-    ElMessage({
-        message: '正在上传',
-        type: 'success'}
-    )
- }catch(err: any){
-    ElMessage({
-        message: err,
-        type: 'error',
-    })
- }
-  
-}}
+const greetMsg = ref("");
+const name = ref("");
+
+async function star_upload() {
+    try{
+        await invoke("start_upload", { req :JSON.stringify({dataset_id: 'xx',server_endpoint: 'xxx' })})
+        message.success('正在上传');
+    }catch(err: any){
+        message.error('上传错误：',err);
+    }
+}
 
 async function stop_upload() {
     try{
         await invoke("stop_upload", { req :JSON.stringify({dataset_id: 'xx' })})
-        ElMessage({
-            message: '暂停上传成功',
-            type: 'success'}
-        )
+        message.success("暂停上传成功");
     }catch(err: any){
-        ElMessage({
-            message: err,
-            type: 'error',
-        })
+        message.error("暂停上传错误：", err);
     }
 }
 
 async function terminate_upload() {
     try{
         await invoke("terminate_upload", { req :JSON.stringify({dataset_id: 'xx' })})
-        ElMessage({
-            message: '终止上传成功',
-            type: 'success'}
-        )
+        message.success("终止上传成功");
     }catch(err: any){
-        ElMessage({
-            message: err,
-            type: 'error',
-        })
+        message.error("终止上传错误：", err);
     }
 }
 
-
 async function get_history() {
-  
- try{
-
-  info("[ui] click get_history btn")
-
-  await invoke("get_history", {req: JSON.stringify({ name:name.value })})
-  ElMessage({
-    message: '获取文件上传历史成功',
-    type: 'success'}
-  )
- }catch(err: any){
-  ElMessage({
-    message: err,
-    type: 'error',
-  })
- }
-  
+    try{
+        info("[ui] click get_history btn")
+        await invoke("get_history", {req: JSON.stringify({ name:name.value })})
+        message.success("获取文件上传历史成功");
+    }catch(err: any){
+        message.error("终止上传错误：", err);
+    }
 }
 
 </script>
-<style scoped>
-.uploadWrapper {
-  width: 60%;
-  margin: 0 auto;
-  margin-top: 70px;
-}
-.fileName{font-size: 14px;}
 
-</style>
+<template>
+  <div class="card">
+    <button type="button" @click="star_upload()">上传文件</button>
+  </div>
+
+  <p>{{ greetMsg }}</p>
+</template>
