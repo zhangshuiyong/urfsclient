abda5286e8bfa4ff3dfd24dd3b47162923c585d9
zhangshuiyong <632386504@qq.com>
2023-07-08 20:47:59 +0800

    get file/folder absolute path in ui layer

--- src/components/upload.vue
+++ src/components/upload.vue
@@ -1,11 +1,42 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { h,ref,reactive } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
+import { open } from '@tauri-apps/api/dialog';
 import { info } from "tauri-plugin-log-api";
 import { message } from 'ant-design-vue';
 
+import { FileOutlined,FolderOutlined } from '@ant-design/icons-vue';
+
 const greetMsg = ref("");
 const name = ref("");
+const uploadItemList:any = reactive([]);
+
+async function select_upload_file() {
+    const selected_file = await open({
+        multiple: false,
+    });
+
+    info("[ui] select upload file :"+selected_file);
+
+    if(typeof selected_file === 'string'){
+        uploadItemList.push({name:selected_file, isDir:false});
+    }
+}
+
+async function select_upload_fold() {
+
+    const selected_folder = await open({
+        multiple: false,
+        directory: true,
+    });
+
+    info("[ui] select upload folder :"+selected_folder);
+
+    if(typeof selected_folder === 'string'){
+        uploadItemList.push({name:selected_folder,isDir:true});
+    }
+}
+
 
 async function star_upload() {
     try{
@@ -48,8 +79,40 @@ async function get_history() {
 
 <template>
   <div class="card">
-    <button type="button" @click="star_upload()">上传文件</button>
+    <button type="button" @click="select_upload_file()">上传文件</button>
+    <button type="button" @click="select_upload_fold()">上传文件夹</button>
+    <button type="button" @click="get_history()">历史任务</button>
   </div>
 
-  <p>{{ greetMsg }}</p>
+  <a-list
+    class="demo-loadmore-list"
+    item-layout="horizontal"
+    :data-source="uploadItemList"
+  >
+    
+    <template #renderItem="{ item }">
+      <a-list-item>
+        <template #actions>
+          <a key="star_upload" @click="star_upload()">开始上传</a>
+          <a key="stop_upload" @click="stop_upload()">暂停上传</a>
+          <a key="terminate_upload" @click="terminate_upload()">终止上传</a>
+        </template>
+       <div>
+          <a-list-item-meta description="">
+            <template #title>
+              <span>{{ item.name }}</span>
+            </template>
+            <template #avatar>
+                <a-avatar size="small" v-if="item.isDir===false">
+                    <template #icon><FileOutlined /></template>
+                </a-avatar>
+                <a-avatar size="small" v-if="item.isDir===true">
+                    <template #icon><FolderOutlined /></template>
+                </a-avatar>
+            </template>
+          </a-list-item-meta>
+        </div>
+      </a-list-item>
+    </template>
+  </a-list>
 </template>
